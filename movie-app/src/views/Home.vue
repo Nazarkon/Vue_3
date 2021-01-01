cde4781a18f0fedcb699fb698fefbeef546dd9a5
Nazarkon <naz.dlcs20@gmail.com>
2021-01-01 19:25:31 +0200

    initialize project

---
+++ movie-app/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="name">{{name}}</div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      name: 'Home',
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.name{
+  color: red
+}
+</style>
